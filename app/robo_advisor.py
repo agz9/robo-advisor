c42939851338db9995e61d3040515571d69c5513
agz9 <77708021+agz9@users.noreply.github.com>
2021-03-06 18:05:17 -0500

    Latest Close and to_usd

--- app/robo_advisor.py
+++ app/robo_advisor.py
@@ -3,6 +3,10 @@
 import requests 
 import json
 
+# function to convert float or int to usd-formatted str
+# credit to shopping cart project 
+def to_usd(my_price):
+    return "${0:,.2f}".format(my_price)
 
 #
 # info outputs 
@@ -18,24 +22,30 @@ response = requests.get(request_url)
 
 parsed_response = json.loads(response.text)
 # converting type string to dictionary so we can work with the text
-last_refreshed=parsed_response["Meta Data"]["3. Last Refreshed"]
+last_refreshed = parsed_response["Meta Data"]["3. Last Refreshed"]
 
-# breakpoint()
+latest_closed = parsed_response["Time Series (Daily)"]["2019-02-20"]["4. close"]
+
+breakpoint()
 
 #
 # info outputs 
 #
+
 
 print("-------------------------")
 print("SELECTED SYMBOL: XYZ")
 print("-------------------------")
 print("REQUESTING STOCK MARKET DATA...")
 print("REQUEST AT: 2018-02-20 02:00pm")
+# date time module from last project 
+#
+#
 print("-------------------------")
 print(f"LATEST DAY: {last_refreshed}")
 # string interpolation using format string
 # could also use concatenation
-print("LATEST CLOSE: $100,000.00")
+print(f"LATEST CLOSE: {to_usd(float(latest_closed))}")
 print("RECENT HIGH: $101,000.00")
 print("RECENT LOW: $99,000.00")
 print("-------------------------")
