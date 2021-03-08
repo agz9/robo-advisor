eee42f939a4d2e2d6ea810270b3fce521d6f7a24
agz9 <77708021+agz9@users.noreply.github.com>
2021-03-08 12:36:28 -0500

    Written rows and looping through each day to csv file

--- app/robo_advisor.py
+++ app/robo_advisor.py
@@ -73,26 +73,17 @@ with open(csv_file_path, "w") as csv_file:
     writer = csv.DictWriter(csv_file, fieldnames = csv_headers)
     # need to get a list of headers from the original url 
     writer.writeheader() # uses fieldnames set above
-
+    for date in dates:
     # some looping here to write each row 
-    writer.writerow({
-        "timestamp": "TODO",
-        "open": "TODO",
-        "high": "TODO",
-        "low": "TODO",
-        "close": "TODO",
-        "volume": "TODO"
-        })
-    
-    writer.writerow({
-        "timestamp": "TODO",
-        "open": "TODO",
-        "high": "TODO",
-        "low": "TODO",
-        "close": "TODO",
-        "volume": "TODO"
-        })
-
+        daily_prices = tsd[date]
+        writer.writerow({
+            "timestamp": "date",
+            "open": daily_prices["1. open"],
+            "high": daily_prices["2. high"],
+            "low": daily_prices["3. low"],
+            "close": daily_prices["4. close"],
+            "volume": daily_prices["5. volume"]
+            })
 
 print("-------------------------")
 print("SELECTED SYMBOL: XYZ")
